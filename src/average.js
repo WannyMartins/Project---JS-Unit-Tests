7f48087185ec9fd19193b0faffaa557d362a29e3
Wanny Martins <wcatery@gmail.com>
2022-01-17 19:02:40 -0300

    requisito 2

--- src/average.js
+++ src/average.js
@@ -11,7 +11,7 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-
+// tive ajudinha do sumo na mentoria pra entender a logica
 const average = (array) => {
   if (array.length === 0) {
     return undefined;
